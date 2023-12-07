ed547e89c56f78a2d02d4978b1ebb222660bec6c
tcdanh2504 <danhtc@foxcode.org>
2023-12-07 20:59:07 +0700

    Refactor: Remove duplicate code in analysis frame

--- ui/analysis_frame.py
+++ ui/analysis_frame.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import ttk, font
 from utils.color import Color
 from controllers.analysis_controller import AnalysisController
-from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
@@ -44,131 +43,65 @@ class AnalysisWindow(tk.Frame):
 
         tab_view = ttk.Notebook(chart_frame)
         
-        self.chart_1(tab_view)
-        self.chart_2(tab_view)
-        self.chart_3(tab_view)
-        self.chart_4(tab_view)
-        self.chart_5(tab_view)
-
-        tab_view.grid(row=0, column=0, sticky='nsew')
+        flights_df = self.controller.get_flights_pd()
+        booking_df = self.controller.get_bookings_pd()
         
-    def chart_1(self, tab_view):
-        tab = ttk.Frame(tab_view, width=980, height=600) 
-        tab_view.add(tab, text='Chart 1')  
-        tab.columnconfigure(0, weight=1)
-        tab.rowconfigure(0, weight=1)
-        tab.grid_rowconfigure(0, weight=1)
-        tab.grid_columnconfigure(0, weight=1)
-        fig = plt.Figure(figsize=(10, 6))
-        ax = fig.add_subplot(111)
-        
-        df = self.controller.get_flights_pd()
-
-        status_counts = df['flight_status'].value_counts()
+        flight_status_fig = plt.Figure(figsize=(10, 6))
+        ax = flight_status_fig.add_subplot(111)
+        status_counts = flights_df['flight_status'].value_counts()
         ax.pie(status_counts, labels=status_counts.index, autopct='%1.1f%%', startangle=140)
         ax.axis('equal') 
         ax.set_title('Flight Status Distribution')
-
-        canvas = FigureCanvasTkAgg(fig, master=tab)
-        tool_bar = NavigationToolbar2Tk(canvas, tab, pack_toolbar=False)
-        tool_bar.grid(sticky='new')
-        canvas.draw()
-        canvas.get_tk_widget().grid(sticky='nsew')
+        self.chart('Flight Status Distribution', tab_view, flight_status_fig)
         
-    def chart_2(self, tab_view):
-        tab = ttk.Frame(tab_view, width=980, height=600) 
-        tab_view.add(tab, text='Chart 2')  
-        tab.columnconfigure(0, weight=1)
-        tab.rowconfigure(0, weight=1)
-        tab.grid_rowconfigure(0, weight=1)
-        tab.grid_columnconfigure(0, weight=1)
-        fig = plt.Figure(figsize=(10, 6))
-        ax = fig.add_subplot(111)
-        
-        df = self.controller.get_flights_pd()
-        airline_counts = df['airline'].value_counts()
+        flights_per_airline_fig = plt.Figure(figsize=(10, 6))
+        ax = flights_per_airline_fig.add_subplot(111)
+        airline_counts = flights_df['airline'].value_counts()
         airline_counts.plot(kind='bar', ax=ax)
-
         ax.set_title('Flights per Airline')
         ax.set_xlabel('Airline')
         ax.set_ylabel('Number of Flights')
         ax.set_xticklabels(ax.get_xticklabels(), rotation=0, horizontalalignment='center')
-
-        canvas = FigureCanvasTkAgg(fig, master=tab)
-        tool_bar = NavigationToolbar2Tk(canvas, tab, pack_toolbar=False)
-        tool_bar.grid(sticky='new')
-        canvas.draw()
-        canvas.get_tk_widget().grid(sticky='nsew')
+        self.chart('Flights per Airline', tab_view, flights_per_airline_fig)
         
-    def chart_3(self, tab_view):
-        tab = ttk.Frame(tab_view, width=980, height=600) 
-        tab_view.add(tab, text='Chart 3')  
-        tab.columnconfigure(0, weight=1)
-        tab.rowconfigure(0, weight=1)
-        tab.grid_rowconfigure(0, weight=1)
-        tab.grid_columnconfigure(0, weight=1)
-        fig = plt.Figure(figsize=(10, 6))
-        ax = fig.add_subplot(111)
-        
-        df = self.controller.get_flights_pd()
-        ax.hist(df['price'], bins=20, edgecolor='black')
-
+        flight_price_distribution_fig = plt.Figure(figsize=(10, 6))
+        ax = flight_price_distribution_fig.add_subplot(111)
+        ax.hist(flights_df['price'], bins=20, edgecolor='black')
         ax.set_title('Flight Price Distribution')
         ax.set_xlabel('Price')
         ax.set_ylabel('Number of Flights')
-
-        canvas = FigureCanvasTkAgg(fig, master=tab)
-        tool_bar = NavigationToolbar2Tk(canvas, tab, pack_toolbar=False)
-        tool_bar.grid(sticky='new')
-        canvas.draw()
-        canvas.get_tk_widget().grid(sticky='nsew')
+        self.chart('Flight Price Distribution', tab_view, flight_price_distribution_fig)
         
-    def chart_4(self, tab_view):
-        tab = ttk.Frame(tab_view, width=980, height=600) 
-        tab_view.add(tab, text='Chart 4')  
-        tab.columnconfigure(0, weight=1)
-        tab.rowconfigure(0, weight=1)
-        tab.grid_rowconfigure(0, weight=1)
-        tab.grid_columnconfigure(0, weight=1)
-        fig = plt.Figure(figsize=(10, 6))
-        ax = fig.add_subplot(111)
-        
-        df = self.controller.get_bookings_pd()
-        df['booking_time'] = pd.to_datetime(df['booking_time'])
-
-        daily_bookings = df.resample('D', on='booking_time').size()
+        bookings_over_time_fig = plt.Figure(figsize=(10, 6))
+        ax = bookings_over_time_fig.add_subplot(111)
+        booking_df['booking_time'] = pd.to_datetime(booking_df['booking_time'])
+        daily_bookings = booking_df.resample('D', on='booking_time').size()
         daily_bookings.plot(ax=ax)
         ax.set_title('Bookings Over Time')
         ax.set_xlabel('Date')
         ax.set_ylabel('Number of Bookings')
-
-        canvas = FigureCanvasTkAgg(fig, master=tab)
-        tool_bar = NavigationToolbar2Tk(canvas, tab, pack_toolbar=False)
-        tool_bar.grid(sticky='new')
-        canvas.draw()
-        canvas.get_tk_widget().grid(sticky='nsew')
+        self.chart('Bookings Over Time', tab_view, bookings_over_time_fig)
         
-    def chart_5(self, tab_view):
-        tab = ttk.Frame(tab_view, width=980, height=600) 
-        tab_view.add(tab, text='Chart 5')  
-        tab.columnconfigure(0, weight=1)
-        tab.rowconfigure(0, weight=1)
-        tab.grid_rowconfigure(0, weight=1)
-        tab.grid_columnconfigure(0, weight=1)
-        fig = plt.Figure(figsize=(10, 6))
-        ax = fig.add_subplot(111)
-        
-        df = self.controller.get_flights_pd()
-        destination_counts = df['destination'].value_counts()
+        top_destinations_fig = plt.Figure(figsize=(10, 6))
+        ax = top_destinations_fig.add_subplot(111)
+        destination_counts = flights_df['destination'].value_counts()
         top_destinations = destination_counts[:10]
-        fig, ax = plt.subplots()
         top_destinations.plot(kind='bar', ax=ax)
-        
         ax.set_title('Top Destinations')
         ax.set_xlabel('Destination')
         ax.set_ylabel('Number of Flights')
         ax.set_xticklabels(ax.get_xticklabels(), rotation=0, horizontalalignment='center')
+        self.chart('Top Destinations', tab_view, top_destinations_fig)
 
+        tab_view.grid(row=0, column=0, sticky='nsew')
+        
+    def chart(self, title, tab_view, fig):
+        tab = ttk.Frame(tab_view, width=980, height=600) 
+        tab_view.add(tab, text=title)  
+        tab.columnconfigure(0, weight=1)
+        tab.rowconfigure(0, weight=1)
+        tab.grid_rowconfigure(0, weight=1)
+        tab.grid_columnconfigure(0, weight=1)
         canvas = FigureCanvasTkAgg(fig, master=tab)
         tool_bar = NavigationToolbar2Tk(canvas, tab, pack_toolbar=False)
         tool_bar.grid(sticky='new')
